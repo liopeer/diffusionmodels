82f0ba3a47b9e7b2a99f9b3d0b3210bebc82df4b
liopeer <lionel.peer@gmail.com>
2023-06-09 00:26:57 +0900

    updated docs

---
+++ models/unet.py
@@ -0,0 +1,108 @@
+import torch.nn as nn
+import torch
+from type_hints.torch import *
+
+class EncodingBlock(nn.Module):
+    def __init__(self, in_channels: int, 
+                 out_channels: int, 
+                 time_embedding_size: int, 
+                 kernel_size: int=3) -> None:
+        """Initialize UNet Encoder Building Block.
+
+        Parameters
+        ---------
+        in_channels
+            number of input channels
+        out_channelse
+            number of output channels
+        time_embedding_size
+            dimension of time embedding
+        kernel_size
+            size of convolutional kernel
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.time_embedding_size = time_embedding_size
+        self.kernel_size = kernel_size
+
+        self.time_embedding_fc = nn.Sequential(
+            nn.Linear(self.time_embedding_size, self.out_channels),
+            nn.ReLU()
+        )
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=self.kernel_size, padding="same"),
+            nn.BatchNorm2d(self.out_channels),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_siez=self.kernel_size, padding="same"),
+            nn.BatchNorm2d(self.out_channels),
+            nn.ReLU()
+        )
+        self.scale = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x: Tensor(Batch, Channels, Height, Width), time_embedding: Tensor(Features)) -> Tensor(Batch, Channels, Height, Width):
+        """Forward pass of UNet Encoder Building Block.
+
+        Parameters
+        ---------
+        x
+            input tensor
+        time_embedding
+            time embedding tensor
+        """
+        time_embedding = self.time_embedding_fc(time_embedding)
+        time_embedding = time_embedding[(..., ) + (None, ) * 2]
+
+        x = self.conv1(x)
+        x = x + time_embedding
+        x = self.conv2(x)
+        x = self.scale(x)
+        return x
+
+class DecodingBlock(EncodingBlock):
+    def __init__(self, in_channels: int,
+                 out_channels: int,
+                 time_embedding_size: int,
+                 kernel_size: int=3) -> None:
+        """Initialize UNet Encoder Building Block.
+
+        Parameters
+        ---------
+        in_channels
+            number of input channels
+        out_channelse
+            number of output channels
+        time_embedding_size
+            dimension of time embedding
+        kernel_size
+            size of convolutional kernel
+        """
+        super().__init__(in_channels, out_channels, time_embedding_size, kernel_size)
+        self.scale = nn.ConvTranspose2d(self.out_channels, self.out_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x: Tensor(Batch, Channels, Height, Width), time_embedding: Tensor(Features)) -> Tensor(Batch, Channels, Height, Width):
+        """Forward pass of UNet Encoder Building Block.
+
+        Parameters
+        ---------
+        x
+            input tensor
+        time_embedding
+            time embedding tensor
+
+        Returns
+        -------
+        Tensor(Batch, Channels, Height, Width)
+            output tensor
+        """
+        return super().forward(x, time_embedding)
+
+class UNet(nn.Module):
+    def __init__(self, num_layers) -> None:
+        super().__init__()
+        self.num_layers = num_layers
+        self.encoding_channels = [64 * (2 ** i) for i in range(self.num_layers)]
+        self.decoding_channels = self.encoding_channels[::-1]
