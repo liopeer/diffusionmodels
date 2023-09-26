bc7af85090be81b2097fd101abf4a38c74a4cf76
liopeer <lionel.peer@gmail.com>
2023-09-26 17:13:19 +0200

    updated diffusion process

--- diffusion_models/models/diffusion.py
+++ diffusion_models/models/diffusion.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn, Tensor
 from jaxtyping import Float, Int64, Int
-from typing import Literal, Tuple
+from typing import Literal, Tuple, Union, List
 from positional_encoding import PositionalEncoding
 
 class ForwardDiffusion(nn.Module):
@@ -45,11 +45,10 @@ class ForwardDiffusion(nn.Module):
             raise NotImplementedError("Invalid scheduler option:", type)
         self.alphas = 1. - self.betas
 
+        self.register_buffer("betas", self.betas, persistent=False)
         self.register_buffer("alphas_dash", torch.cumprod(self.alphas, axis=0), persistent=False)
         self.register_buffer("sqrt_alphas_dash", torch.sqrt(self.alphas_dash), persistent=False)
         self.register_buffer("sqrt_one_minus_alpha_dash", 1. - self.alphas_dash, persistent=False)
-
-        self.register_buffer("noise_normal", torch.empty((1)), persistent=False)
 
     def forward(
             self, 
@@ -70,14 +69,14 @@ class ForwardDiffusion(nn.Module):
         Float[Tensor, "batch channels height width"]
             tensor with applied noise according to schedule and chosen timestep
         """
-        self.noise_normal = torch.randn_like(x_0)
+        noise_normal = torch.randn_like(x_0, device=x_0.device)
         if True in torch.gt(t, self.timesteps-1):
             raise IndexError("t ({}) chosen larger than max. available t ({})".format(t, self.timesteps-1))
         sqrt_alpha_dash_t = self.sqrt_alphas_dash[t]
         sqrt_one_minus_alpha_dash_t = self.sqrt_one_minus_alpha_dash[t]
         x_t = sqrt_alpha_dash_t.view(-1, 1, 1, 1) * x_0
-        x_t += sqrt_one_minus_alpha_dash_t.view(-1, 1, 1, 1) * self.noise_normal
-        return x_t, self.noise_normal
+        x_t += sqrt_one_minus_alpha_dash_t.view(-1, 1, 1, 1) * noise_normal
+        return x_t, noise_normal
 
     def _linear_scheduler(self, timesteps, start, end):
         return torch.linspace(start, end, timesteps)
@@ -91,10 +90,25 @@ class DiffusionModel(nn.Module):
             self,
             backbone: nn.Module,
             fwd_diff: ForwardDiffusion,
+            img_size: int,
             time_enc_dim: int=256,
-            dropout: float=0
+            dropout: float=0,
         ) -> None:
-        """"""
+        """Constructor of DiffusionModel class.
+
+        Parameters
+        ----------
+        backbone
+            backbone module (instance) for noise estimation
+        fwd_diff
+            forward diffusion module (instance)
+        img_size
+            size of (quadratic) images
+        time_enc_dim
+            feature dimension that should be used for time embedding/encoding
+        dropout
+            value of dropout layers
+        """
         super().__init__()
         self.model = backbone
         self.fwd_diff = fwd_diff
@@ -119,50 +133,58 @@ class DiffusionModel(nn.Module):
         out
             tuple of noise predictions and noise for random timesteps in the denoising process
         """
-        self.timesteps = self._sample_timesteps(x.shape[0]).to(x.device)
+        self.timesteps = self._sample_timesteps(x.shape[0], device=x.device)
         self.time_enc = self.time_enc.get_pos_encoding(self.timesteps)
         x_t, noise = self.fwd_diff(x, self.timesteps)
         noise_pred = self.model(x_t, self.time_enc)
         return noise_pred, noise
     
-    def sample(self, n: int, img_size: Tuple[int, int]):
-        """Sample a batch of images."""
+    def sample_batch(
+            self, 
+            num_samples: int, 
+            debugging: bool=False,
+            save_every: int=20
+        ) -> Union[Float[Tensor, "batch channel height width"], List[Float[Tensor, "batch channel height width"]]]:
+        """Sample a batch of images.
+
+        Parameters
+        ----------
+        num_samples
+            how big the batch should be
+        debugging
+            if true, returns list that shows the sampling process
+        save_every
+            defines how often the tensors should be saved in the denoising process
+
+        Returns
+        -------
+        out
+            either a list of tensors if debugging is true, else a single tensor with final images
+        """
         self.model.eval()
         device = list(self.parameters())[0].device
         with torch.no_grad():
-            x = torch.randn((n, self.model.in_channels, img_size, img_size), device=device)
+            x = torch.randn((num_samples, self.model.in_channels, self.img_size, self.img_size), device=device)
+            x_list = []
             for i in reversed(range(1, self.fwd_diff.timesteps)):
-                t_steps = torch.ones((n), dtype=torch.long, device=device)
-                t_enc = self._time_encoding(t_steps, self.time_enc_dim)
-                predicted_noise = self.model(x, t_enc)
+                t_step = i * torch.ones((num_samples), dtype=torch.long, device=device)
+                t_enc = self.time_enc.get_pos_encoding(t_step)
+                noise_pred = self.model(x, t_enc)
 
-                self.fwd_diff.alphas = self.fwd_diff.alphas.to(device)
-                self.fwd_diff.alphas_dash = self.fwd_diff.alphas_dash.to(device)
-                self.fwd_diff.betas = self.fwd_diff.betas.to(device)
-
-                alpha = self.fwd_diff.alphas[t_steps][:, None, None, None]
-                alpha_hat = self.fwd_diff.alphas_dash[t_steps][:, None, None, None]
-                beta = self.fwd_diff.betas[t_steps][:, None, None, None]
+                alpha = self.fwd_diff.alphas[t_step][:, None, None, None]
+                alpha_hat = self.fwd_diff.alphas_dash[t_step][:, None, None, None]
+                beta = self.fwd_diff.betas[t_step][:, None, None, None]
                 if i > 1:
                     noise = torch.randn_like(x, device=device)
                 else:
                     noise = torch.zeros_like(x, device=device)
-                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * noise_pred) + torch.sqrt(beta) * noise
+                if debugging and (i % save_every == 0):
+                    x_list.append(x)
         self.model.train()
+        if debugging:
+            return x_list
         return x
-    
-    def _time_encoding(
-            self, 
-            t: Int[Tensor, "batch"], 
-            channels: int
-        ) -> Float[Tensor, "batch time_enc_dim"]:
-        t = t.unsqueeze(-1).type(torch.float)
-        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
-        inv_freq = inv_freq.to(t.device)
-        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
-        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
-        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
-        return pos_enc
 
-    def _sample_timesteps(self, batch_size: int) -> Int64[Tensor, "batch"]:
-        return torch.randint(low=1, high=self.fwd_diff.timesteps, size=(batch_size,))
+    def _sample_timesteps(self, batch_size: int, device: torch.device) -> Int64[Tensor, "batch"]:
+        return torch.randint(low=1, high=self.fwd_diff.timesteps, size=(batch_size,), device=device)
